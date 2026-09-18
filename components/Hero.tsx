1d6ac6ec86133dddaddf16b3cebbf4c4f2003db5
NiranjanD0 <niranjan4281@gmail.com>
2026-09-19 03:38:24 +0530

    feat: Added icon & background

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const Hero = () => {
+  return (
+    <section
+      className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-screen bg-[url('/assets/images/main.gif')] bg-cover bg-center"
+    >
+    </section>
+  );
+};
+
+export default Hero;
